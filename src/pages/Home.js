9954929d72249779006c31616673e1d20ba1b1cc
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2023-11-19 22:01:53 +0800

    Update home page and metadata

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import GithubIcon from "@mui/icons-material/GitHub";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My Name is Renzo</h2>
+        <div className="prompt">
+          <p>A Computer Engineering student from the National University of Singapore 
+            exploring Machine Learning, AI and Hardware & Software Engineering.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Software Engineering</h2>
+            <span>
+              Insert skills here.
+            </span>
+          </li>
+          <li className="item">
+            <h2>Hardware Engineering</h2>
+            <span>
+              Insert skills here.
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>C, C++, C#, Java, Python</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
