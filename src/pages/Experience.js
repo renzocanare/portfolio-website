a2ae77f311bf1b3dbae05d057c2c7a5c16e28af2
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2023-11-19 22:29:34 +0800

    Add base code to experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
 
 function Experience() {
   return (
-    <div>Experience</div>
-  )
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"     
+          date="In Progress"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            HP Inc. Singapore
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            R&D Firmware Engineering Intern
+          </h4>
+          <p>Insert description.</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2020 - present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Job
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Role
+          </h4>
+          <p>
+            Insert description.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2018 - 2020"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          National University of Singapore
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor's Degree
+          </h4>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2014 - 2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            School
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Role
+          </h4>
+          <p> Insert description.</p>
+        </VerticalTimelineElement>
+
+      </VerticalTimeline>
+    </div>
+  );
 }
 
-export default Experience
+export default Experience;
