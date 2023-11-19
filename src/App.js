6ca7e335f8943ede3f75af04e460ac2cef9d997f
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2023-11-19 22:49:32 +0800

    Add framework for projects page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
+import ProjectDisplay from "./pages/ProjectDisplay";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experience" element={<Experience />} />          
         </Routes>
       </Router>
