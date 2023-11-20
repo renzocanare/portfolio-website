a9cb98f1270c7812d19f8212340a2598bb12961d
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2023-11-20 14:46:19 +0800

    Fix bug where ScrollButton will have a ghosting effect on mobile devices

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -1,38 +1,40 @@
-import React, {useState} from 'react'; 
-import {FaArrowCircleUp} from 'react-icons/fa'; 
-import { Button } from '../styles/ScrollButton'; 
-  
-const ScrollButton = () =>{ 
-  
-  const [visible, setVisible] = useState(false) 
-  const pageScroll = 500
-  const toggleVisible = () => { 
-    const scrolled = document.documentElement.scrollTop; 
-    if (scrolled > pageScroll){ 
-      setVisible(true) 
-    }  
-    else if (scrolled <= pageScroll){ 
-      setVisible(false) 
-    } 
-  }; 
-  
-  const scrollToTop = () =>{ 
-    window.scrollTo({ 
-      top: 0,  
-      behavior: 'smooth'
-      /* you can also use 'auto' behaviour 
-         in place of 'smooth' */
-    }); 
-  }; 
-  
-  window.addEventListener('scroll', toggleVisible); 
-  
-  return ( 
-    <Button> 
-     <FaArrowCircleUp onClick={scrollToTop}  
-     style={{display: visible ? 'inline' : 'none'}} /> 
-    </Button> 
-  ); 
-} 
-  
-export default ScrollButton; 
+import React, { useState, useEffect } from 'react';
+import { FaArrowCircleUp } from 'react-icons/fa';
+import { Button } from '../styles/ScrollButton';
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+  const pageScroll = 500;
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > pageScroll) {
+      setVisible(true);
+    } else if (scrolled <= pageScroll) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisible);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisible);
+    };
+  }, []);
+
+  return (
+    <Button onClick={scrollToTop} style={{ display: visible ? 'inline' : 'none' }}>
+      <FaArrowCircleUp />
+    </Button>
+  );
+};
+
+export default ScrollButton;
