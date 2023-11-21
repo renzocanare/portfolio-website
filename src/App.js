7c40e2ab809b1a64ecea9f708ab0820d40298664
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2023-11-22 01:15:57 +0800

    Remove ProjectDisplay since we are using modals for Projects now

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
-import ProjectDisplay from "./pages/ProjectDisplay";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollButton from './components/ScrollButton'; 
@@ -19,7 +18,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experience" element={<Experience />} />          
         </Routes>
       </Router>
