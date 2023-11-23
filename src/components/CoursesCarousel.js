937a560cc86ad01724bf05dc6624499aeb74712c
Renzo Canare <69526313+renzocanare@users.noreply.github.com>
2023-11-24 01:26:40 +0800

    Rename ClassesList to CoursesList

--- src/components/CoursesCarousel.js
+++ src/components/CoursesCarousel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 import ClassesCard from "./ClassesCard"; 
-import { ClassesList } from "../helpers/ClassesList"; 
+import { ClassesList } from "../helpers/CoursesList"; 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
